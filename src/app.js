99fce3d8df1c989d4a01f3d89b2330aa0826d634
Devin McGinty <devin.lee.mcginty@gmail.com>
2015-01-10 16:30:29 -0500

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ testing github integration
+ Creep Button
+ *
+ */
+
+var UI = require('ui');
+var Vibrate = require('ui/vibe');
+var Vector = require('vector2');
+var Accel = require('ui/accel');
+
+Accel.on('tap', function(e) {
+  // something goes here
+});
+
+Accel.init();
